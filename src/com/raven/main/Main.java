134b981fabf433fb23eab667d5cede5306fa2ce4
thinhzzhb <dafuq412@gmail.com>
2024-03-20 01:20:25 +0700

    upload project

---
+++ src/com/raven/main/Main.java
@@ -0,0 +1,166 @@
+package com.raven.main;
+
+import com.raven.event.EventMenu;
+import com.raven.form.Form;
+import com.raven.form.Form_1;
+import com.raven.form.Giao_dich;
+import com.raven.form.Nhan_Vien;
+import com.raven.form.San_Pham;
+import com.raven.form.login_Form;
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JOptionPane;
+
+public class Main extends javax.swing.JFrame {
+
+    public Main() {
+        initComponents();
+        setBackground(new Color(0, 0, 0, 0));
+        EventMenu event = new EventMenu() {
+            @Override
+            public void selected(int index) {
+//                if (index == 0) {
+//                    showForm(new Form_1());
+//                } else if (index == 8) {
+//                    System.exit(0);
+//                } else {
+//                    showForm(new Form(index));
+//                }
+                switch (index) {
+                    case 0:
+                        showForm(new Form(index));
+                        break;
+                    case 1:
+                        showForm(new San_Pham());
+                        break;
+                    case 2:
+                        showForm(new Nhan_Vien());
+                        break;
+                    case 3:
+                        showForm(new Giao_dich());
+                        break;
+                    case 4:
+                        showForm(new Form(index));
+                        break;
+                    case 5:
+                        showForm(new Form_1());
+                        break;
+                    case 6:
+                        showForm(new Form(index));
+                        break;
+                    case 7:
+                        showForm(new Form(index));
+                        break;
+                    case 8:
+                        System.exit(0);
+                        break;
+                    default:
+                        
+                }
+            }
+        };
+        menu1.initMenu(event);
+        showForm(new Form(0));
+    }
+
+    private void showForm(Component com) {
+        body.removeAll();
+        body.add(com);
+        body.revalidate();
+        body.repaint();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        roundPanel1 = new com.raven.swing.RoundPanel();
+        header2 = new com.raven.component.Header();
+        menu1 = new com.raven.component.Menu();
+        body = new javax.swing.JPanel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+
+        roundPanel1.setBackground(new java.awt.Color(255, 204, 102));
+
+        body.setOpaque(false);
+        body.setLayout(new java.awt.BorderLayout());
+
+        javax.swing.GroupLayout roundPanel1Layout = new javax.swing.GroupLayout(roundPanel1);
+        roundPanel1.setLayout(roundPanel1Layout);
+        roundPanel1Layout.setHorizontalGroup(
+            roundPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(header2, javax.swing.GroupLayout.DEFAULT_SIZE, 1371, Short.MAX_VALUE)
+            .addGroup(roundPanel1Layout.createSequentialGroup()
+                .addGap(10, 10, 10)
+                .addComponent(menu1, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(body, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(10, 10, 10))
+        );
+        roundPanel1Layout.setVerticalGroup(
+            roundPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(roundPanel1Layout.createSequentialGroup()
+                .addComponent(header2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(10, 10, 10)
+                .addGroup(roundPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(menu1, javax.swing.GroupLayout.DEFAULT_SIZE, 756, Short.MAX_VALUE)
+                    .addComponent(body, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(10, 10, 10))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(roundPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(roundPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Main().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel body;
+    private com.raven.component.Header header2;
+    private com.raven.component.Menu menu1;
+    private com.raven.swing.RoundPanel roundPanel1;
+    // End of variables declaration//GEN-END:variables
+}
